31d2160ad100b53c05e771e2cfddbb4569fa9fef
NILANJAN DEB <nilanjan172nsvian@gmail.com>
2024-01-24 23:23:19 +0530

    remove cloudflare container

--- add-trino-cluster.sh
+++ add-trino-cluster.sh
@@ -6,7 +6,7 @@ curl \
   -H "Content-Type: application/json" \
   -X POST localhost:9080/gateway/backend/modify/add \
   -d '{ "name": "trino-1",
-        "proxyTo": "https://docker-trino-1.nilanjandeb.com",
+        "proxyTo": "http://trino-cluster-1:8080",
         "active": true,
         "routingGroup": "adhoc"
       }'
@@ -15,7 +15,7 @@ curl \
   -H "Content-Type: application/json" \
   -X POST localhost:9080/gateway/backend/modify/add \
   -d '{ "name": "trino-2",
-        "proxyTo": "https://docker-trino-2.nilanjandeb.com",
+        "proxyTo": "http://trino-cluster-2:8080",
         "active": true,
         "routingGroup": "adhoc"
       }'
